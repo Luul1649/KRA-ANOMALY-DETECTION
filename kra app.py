c3b52f055977c932587e17f3a3715ba4c62134e1
Luul1649 <luulhaji1649@gmail.com>
2026-05-30 10:46:58 +0300

    Update kra app.py

--- kra app.py
+++ kra app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import sklearn.ensemble
+import sklearn.model_selection
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
